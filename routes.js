83f28bc651a9d5ada22c0683eef8ed5ddb284f72
pnkazlou <pnkazlou@ibagroup.eu>
2021-10-18 17:19:45 +0300

    test

--- routes.js
+++ routes.js
@@ -65,56 +65,5 @@ server.get('/main/:id', (req, res) => {
 server.get('/test', (_req, res) => {
 	res.end("HEllo")
   });
-// server.post('/dodo', (req, res) => {
-// 	const { name, age } = req.body;
-// 	let newUser = new User();
-// 	newUser.name = name;
-// 	newUser.age = age;
-// 	newUser.save((err, data) => {
-// 	  if (err) {
-// 		return res.status(400).json({
-// 		  error: err
-// 		});
-// 	  }
-// 	  let newCar = new Car({ marka: 'fiat', hp: '123' });
-// 	  newCar.save((err, data) => {
-// 		if (err) {
-// 		  return res.status(400).json({
-// 			error: err
-// 		  });
-// 		}
-// 		res.json(data);
-// 	  });
-// 	  res.json(data);
-// 	});
-//   });
-
-//   server.get('/dodo', (_req, res) => {
-// 	User.find({}).exec((err, data) => {
-// 	  if (err) return res.status(400).json({ error });
-// 	  res.json(data);
-// 	});
-//   });
-
-// app.post("/users", async (request, response) => {
-//   const user = new userModel(request.body);
-
-//   try {
-//     await user.save();
-//     response.send(user);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
-
-// app.get("/users", async (request, response) => {
-//     const users = await userModel.find({});
-
-//     try {
-//       response.send(users);
-//     } catch (error) {
-//       response.status(500).send(error);
-//     }
-//   });
 
 module.exports = server;

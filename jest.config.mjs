3cbf1a0261d8f169998e40a48349c6b1725fad9d
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2024-09-27 18:08:02 +0300

    fix

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,8 @@
-// module.exports = {
-//   preset: 'jest',
-//   testEnvironment: 'node',
-//   testRegex: "__tests__/.*.e2e.test.js$",
-// }
-
 /** @type {import('jest').Config} */
 export default {
-  testEnvironment: 'node',
+  testEnvironment: "node",
   testRegex: "__tests__/.*\\.e2e\\.test\\.js$", // Adjust regex to match your test files
-  moduleFileExtensions: ['js', 'json', 'node'],
+  moduleFileExtensions: ["js", "json", "node"],
   transform: {
     "^.+\\.js$": "babel-jest", // Use Babel to transpile your test files
   },
